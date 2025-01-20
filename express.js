e01e262062c0095608af729eac6228334c195773
icrucialx <avion.blueberry@gmail.com>
2025-01-20 16:21:37 -0600

    Update express.js

--- express.js
+++ express.js
@@ -5,29 +5,25 @@ const PORT = process.env.PORT || 3000;
 // Import card pool JSON
 const cardPool = require("./card_pool.json");
 
-app.get("/pull", async (req, res) => {
-    const username = req.query.username;
+app.get('/pull', (req, res) => {
+    const username = req.query.username || 'Guest';
 
-    if (!username) {
-        return res.status(400).send("Error: Username is required");
-    }
-
-    const cardPool = require("./card_pool.json");
+    const cardPool = require('./card_pool.json');
     const rarities = Object.keys(cardPool);
 
-    // Function to pull a random card
     const pullRandomCard = () => {
-        const rarity = rarities[Math.floor(Math.random() * rarities.length)];
-        const card = cardPool[rarity][Math.floor(Math.random() * cardPool[rarity].length)];
-        return { card, rarity };
+      const rarity = rarities[Math.floor(Math.random() * rarities.length)];
+      const card = cardPool[rarity][Math.floor(Math.random() * cardPool[rarity].length)];
+      return { card, rarity };
     };
 
-    // Pull three cards
     const pulls = [pullRandomCard(), pullRandomCard(), pullRandomCard()];
 
-    // Format the response
-    const pullsFormatted = pulls.map(p => `${p.card} (${p.rarity})`).join(", ");
-    res.send(`@${username}, you pulled: ${pullsFormatted}`);
+    res.json({
+        success: true,
+        username: username,
+        pulls: pulls
+    });
 });
 
 app.get("/collection", async (req, res) => {
